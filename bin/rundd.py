16bcf442c0228455952a26d4c7464d2edaa15e7b
bpe54.thales <bpe54@thales>
2026-05-21 00:08:56 +0300

    updates

--- bin/rundd.py
+++ bin/rundd.py
@@ -596,9 +596,9 @@ def link2campaign(options, dt, add2temp_files=True):
         dest = os.path.join(PDIR, 'STA', os.path.basename(src))
         link_dict.append({'src': src, 'dest': dest})
 
-    ## regional crd file (linked to REG$YSS+0)
+    ## regional crd file (linked to REG_$YSS+0)
     src = os.path.join(TDIR, 'crd', options['aprinf'].upper()+'.CRD')
-    dest = os.path.join(PDIR, 'STA', 'REG{:}0.CRD'.format(dt.strftime("%y%j")))
+    dest = os.path.join(PDIR, 'STA', 'REG_{:}0.CRD'.format(dt.strftime("%Y%j")))
     link_dict.append({'src': src, 'dest': dest})
 
     ## sta file
@@ -1528,18 +1528,18 @@ if __name__ == '__main__':
 
     ## compile a quick report based on the ADDNEQ2 output file for every
     ## station (appended to the log-file)
-    if not bpe_error:
-        compile_report(options, dt, logfn, netsta_dct, station_ts_updated, rinex_holdings)
+#    if not bpe_error:
+#        compile_report(options, dt, logfn, netsta_dct, station_ts_updated, rinex_holdings)
 
     ## assert that all stations (RINEX) downloaded are indeed included in the
     ## processing
-    if not bpe_error:
-        check_downloaded_are_processed(rinex_holdings, os.path.join(os.getenv('P'), options['campaign'].upper(), 'OUT', '{:}{:}0.OUT'.format(solution_id['final'], dt.strftime('%y%j'))), logfn)
+#    if not bpe_error:
+#        check_downloaded_are_processed(rinex_holdings, os.path.join(os.getenv('P'), options['campaign'].upper(), 'OUT', '{:}{:}0.OUT'.format(solution_id['final'], dt.strftime('%y%j'))), logfn)
 
     ## collect warning messages in a list (of dictionaries for every warning)
-    if not bpe_error:
-        warning_messages = bpe.collect_warning_messages(os.path.join(os.getenv('P'), options['campaign'].upper()), dt.strftime('%j'), bpe_start_at, bpe_stop_at)
-        compile_warnings_report(warning_messages, logfn)
+#    if not bpe_error:
+#        warning_messages = bpe.collect_warning_messages(os.path.join(os.getenv('P'), options['campaign'].upper()), dt.strftime('%j'), bpe_start_at, bpe_stop_at)
+#        compile_warnings_report(warning_messages, logfn)
 
     ## upload SINEX files if needed (SINEX to EPND ftp)
     if not bpe_error and options['upload_to_epnd']:
