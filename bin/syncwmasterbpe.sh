639cb6b8ac8ca4fa0d63df5102ca8312b48dd858
bpe54.thales <bpe54@thales>
2026-06-02 16:42:33 +0300

    update sync master

--- bin/syncwmasterbpe.sh
+++ bin/syncwmasterbpe.sh
@@ -4,35 +4,42 @@
 
 ## Help Function
 function help {
-    echo " Usage  : ./syncwmasterbpe.sh --host xxx.xxx.xxx.xxx --user bpe54"
+    echo " Usage  : ./syncwmasterbpe.sh --host xxx.xxx.xxx.xxx --user bpe54 --campaign CAMPAIGN_NAME"
     exit 1
 }
 
-if [ "$#" -ne 4 ]
-then
-	echo "[ERROR] : No input argument"
-	help
+if [ "$#" -lt 2 ]; then
+    echo "[ERROR] : Missing required arguments"
+    help
 fi
 
-while [ $# -eq 4 ]
-do 
+while [ $# -gt 0 ]
+do
     case "$1" in
-	--host)
-	    HOST=${2}
-	    shift 2
-	    ;;
-	--user)
-	    USER=${2}
-	    shift 2
-	    ;;
-    	*)
-            echo "[ERROR] Wronk arguments"
-	    help
-	    exit 1
+        --host)
+            HOST=${2}
+            shift 2
+            ;;
+        --user)
+            USER=${2}
+            shift 2
+            ;;
+        --campaign)
+            CAMPAIGN=${2}
+            shift 2
+            ;;
+        *)
+            echo "[ERROR] Wrong arguments"
+            help
+            exit 1
+            ;;
     esac
 done
 
-
+if [ -z "${HOST}" ] || [ -z "${USER}" ]; then
+    echo "[ERROR] --host and --user are required"
+    help
+fi
 
 R_TABLES="${USER}@${HOST}:/home/bpe54/tables/"
 L_TABLES="/home/bpe54/tables/"
@@ -56,14 +63,19 @@ rsync -a -z \
     --progress \
     "$R_GPSU" "$L_GPSU"
 
-R_GEN="${USER}@${HOST}:/home/bpe54/data/GPSDATA/CAMPAIGN54/GREECE/GEN"
-L_GEN="/home/bpe54/data/GPSDATA/CAMPAIGN54/GREECE/GEN"
 
-rsync -a -z \
-    --update \
-    --partial \
-    --append-verify \
-    --human-readable \
-    --progress \
-    "$R_GEN" "$L_GEN"
+if [ -n "${CAMPAIGN}" ]; then
+    R_GEN="${USER}@${HOST}:/home/bpe54/data/GPSDATA/CAMPAIGN54/${CAMPAIGN}/GEN/"
+    L_GEN="/home/bpe54/data/GPSDATA/CAMPAIGN54/${CAMPAIGN}/GEN/"
+
+    rsync -a -z \
+        --update \
+        --partial \
+        --append-verify \
+        --human-readable \
+        --progress \
+        "$R_GEN" "$L_GEN"
+else
+    echo "[INFO] --campaign not provided; skipping GEN synchronization"
+fi
 
