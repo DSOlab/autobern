188a9963f5b8b3ea98d4b93de1871b2be594a7c7
bpe54.thales <bpe54@thales>
2026-08-15 16:14:00 +0300

    go for b63

--- pybern/setup.py
+++ pybern/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='pybern',
-    version='1.6-b62,
+    version='1.6-b63',
     description='Python Modules to Assist Bernese-based GNSS processing (upd for BERN54)',
     url='',
     author='Xanthos Papanikolaou, Dimitris Anastasiou',
